b285bab80ff42f9024df3d092b8c3d5200036c75
Jess Perkins <starkos@industriousone.com>
2024-10-01 15:41:38 -0400

    Fix issues discovered in first uses

--- macos/AppDelegate.cs
+++ macos/AppDelegate.cs
@@ -5,15 +5,18 @@ public class AppDelegate : NSApplicationDelegate
 {
 	private readonly MainWindowController _mainWindowController = new ();
 
+
 	public override void DidFinishLaunching (NSNotification notification)
 	{
 		_mainWindowController.ShowWindow(this);
 	}
 
+
 	public override Boolean SupportsSecureRestorableState (NSApplication application)
 	{
 		return true;
 	}
+
 
 	public override void WillTerminate (NSNotification notification)
 	{
